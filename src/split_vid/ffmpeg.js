e49c7ed7b6fbbb87ed7a5eb0e98ec17879fd5d44
amethystcoder <amethystguitaristj@gmail.com>
2024-05-16 20:04:22 +0100

    feature: added video resolution code

--- src/split_vid/ffmpeg.js
+++ src/split_vid/ffmpeg.js
@@ -9,9 +9,14 @@ fluentFfmpeg(`../hls_files/${fileName}.mp4`, {timeout: 43200}).addOptions([
     '-profile:v baseline',
     '-level 3.0',
     '-start_number 0',
-    '-hls_time 5',
+    '-hls_time 10',
     '-hls_list_size 0',
     '-f hls'
-]).output(`videos/${fileName}.m3u8`).on('end', () => {
-    console.log('read')
+]).output(`videos/${fileName}.m3u8`)
+.on('error', (err) => {
+    console.log('an error occured processing the file.\n err:',err)
+}).on('progress', (progress) => {
+    console.log(`progress: ${progress.frames } frames`)
+}).on('end', () => {
+    console.log('processing completed')
 }).run()
---
+++ src/split_vid/separate_video_qualities.js
@@ -0,0 +1,31 @@
+const fluentFfmpeg = require('fluent-ffmpeg')
+//const exec = require('child_process').exec
+const Ffmpeg = require('@ffmpeg-installer/ffmpeg')
+
+fluentFfmpeg.setFfmpegPath(Ffmpeg.path)
+
+let fileName = 'Alan_Walker_-_Faded' //variable data...
+fluentFfmpeg(`../hls_files/${fileName}.mp4`)
+.outputOptions('-vf',"scale=1280:720")
+.saveToFile(`../hls_files/${fileName}-1080p.mp4`)
+
+/* .output(`../hls_files/${fileName}-480p.mp4`)//upsize 480p
+.videoCodec('libx264')
+.size('640x480') */
+
+/* .output(`../hls_files/${fileName}-720p.mp4`)//upsize 720p
+.videoCodec('libx264')
+.size('1280x720')
+
+.output(`../hls_files/${fileName}-1080p.mp4`)//upsize 1080p
+.videoCodec('libx264')
+.size('1920x1080') */
+
+.on('error', (err) => {
+    console.log('an error occured processing the file.\n err:',err)
+}).on('progress', (progress) => {
+    console.log(`progress: ${progress.frames } frames`)
+}).on('end', () => {
+    console.log('processing completed')
+})
+
