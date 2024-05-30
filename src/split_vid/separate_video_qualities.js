89c6876d65b14ff0b69c9678c448b8f21bff1154
amethystcoder <amethystguitaristj@gmail.com>
2024-05-30 19:12:11 +0100

    installed new modules and added files

--- src/split_vid/separate_video_qualities.js
+++ src/split_vid/separate_video_qualities.js
@@ -1,13 +1,16 @@
 const fluentFfmpeg = require('fluent-ffmpeg')
-//const exec = require('child_process').exec
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 const Ffmpeg = require('@ffmpeg-installer/ffmpeg')
 
 fluentFfmpeg.setFfmpegPath(Ffmpeg.path)
 
 let fileName = 'Alan_Walker_-_Faded' //variable data...
-fluentFfmpeg(`../hls_files/${fileName}.mp4`)
+
+(()=>{})
+/* fluentFfmpeg(`../hls_files/${fileName}.mp4`)
 .outputOptions('-vf',"scale=1280:720")
-.saveToFile(`../hls_files/${fileName}-1080p.mp4`)
+.saveToFile(`../hls_files/${fileName}-1080p.mp4`) */
 
 /* .output(`../hls_files/${fileName}-480p.mp4`)//upsize 480p
 .videoCodec('libx264')
@@ -21,11 +24,11 @@ fluentFfmpeg(`../hls_files/${fileName}.mp4`)
 .videoCodec('libx264')
 .size('1920x1080') */
 
-.on('error', (err) => {
+/* .on('error', (err) => {
     console.log('an error occured processing the file.\n err:',err)
 }).on('progress', (progress) => {
     console.log(`progress: ${progress.frames } frames`)
 }).on('end', () => {
     console.log('processing completed')
-})
 
+}) */
