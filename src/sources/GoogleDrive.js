517afa18c775ed782ec5faab45381db1b1b35c23
amethystcoder <amethystguitaristj@gmail.com>
2024-06-02 20:33:26 +0100

    new files and config

--- src/sources/GoogleDrive.js
+++ src/sources/GoogleDrive.js
@@ -1,2 +1,14 @@
-const googleApis = require('googleapis') 
+const googleApis = require('googleapis')
+const axios = require('axios')
+const env = require('dotenv').config()
 
+
+/**
+ * gets the source of the 
+ */
+const getSource = async (url,id) =>{
+    const res = await axios({
+        method: 'get',
+        url: ''
+    })
+}
